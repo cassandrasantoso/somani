e988cfd8e715fa22baf7b3de7a4b5518dcc61d5e
Cassandra Santoso <cassandra.santoso04@gmail.com>
2026-08-08 16:55:58 +0900

    message.rb updated

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,12 @@
 class Message < ApplicationRecord
+  ROLES = %w[user assistant].freeze
+
   belongs_to :adventure
+  has_one :feedback, dependent: :destroy
+  has_one_attached :audio
+
+  validates :role, inclusion: { in: ROLES }
+  validates :body, presence: true
+
+  scope :chronological, -> { order(:created_at) }
 end
