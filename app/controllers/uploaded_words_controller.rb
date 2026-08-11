fa77d92a628580b9936bcb17522d414554b9405c
Cassandra Santoso <cassandra.santoso04@gmail.com>
2026-08-11 16:41:38 +0900

    uploaded_words_controller.rb updated

--- app/controllers/uploaded_words_controller.rb
+++ app/controllers/uploaded_words_controller.rb
@@ -1,4 +1,10 @@
 class UploadedWordsController < ApplicationController
+  # Removes a word from ONE upload. The word itself survives.
   def destroy
+    @uploaded_word = UploadedWord.find(params[:id])
+    authorize @uploaded_word
+    upload = @uploaded_word.upload
+    @uploaded_word.destroy
+    redirect_to upload, status: :see_other
   end
 end
