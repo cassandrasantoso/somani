dbca2dac7697fd2e270461b92c1ce59ac1bc21a1
Nina Galindo <ninalorettagalindo@gmail.com>
2026-09-03 16:52:21 +0900

    changed the ui of the listen button and added the function of pause and resume

--- app/javascript/controllers/audio_player_controller.js
+++ app/javascript/controllers/audio_player_controller.js
@@ -2,11 +2,48 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static values = { url: String }
+  static targets = ["button", "icon", "label"]
+
+  connect() {
+    this.audio = new Audio(this.urlValue)
+
+    this.audio.addEventListener("ended", () => {
+      this.reset()
+    })
+  }
+
+  toggle() {
+    if (this.audio.paused) {
+      this.play()
+    } else {
+      this.pause()
+    }
+  }
 
   play() {
-    if (!this.audio) {
-      this.audio = new Audio(this.urlValue)
-    }
     this.audio.play()
+
+    this.buttonTarget.classList.add("is-playing")
+    this.iconTarget.textContent = "❚❚"
+    this.labelTarget.textContent = "Pause"
+  }
+
+  pause() {
+    this.audio.pause()
+
+    this.buttonTarget.classList.remove("is-playing")
+    this.buttonTarget.classList.add("is-paused")
+
+    this.iconTarget.textContent = "▶"
+    this.labelTarget.textContent = "Resume"
+  }
+
+  reset() {
+    this.audio.currentTime = 0
+
+    this.buttonTarget.classList.remove("is-playing", "is-paused")
+
+    this.iconTarget.textContent = "▶"
+    this.labelTarget.textContent = "Listen"
   }
 }
